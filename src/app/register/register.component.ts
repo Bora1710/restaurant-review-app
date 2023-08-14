2a7fbc2effd9c3e47020941e87a0b57ff27bbc81
Bora1710 <milosevicb92@gmail.com>
2023-08-14 19:44:01 +0200

    create Register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
 }
