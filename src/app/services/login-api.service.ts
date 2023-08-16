427ada2356f0fcc0dabb580e02e710dee1bd4a66
Bora1710 <milosevicb92@gmail.com>
2023-08-16 19:04:13 +0200

    Fix models path

--- src/app/services/login-api.service.ts
+++ src/app/services/login-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
-import { HttpResponse } from '../shared/models/http';
+import { HttpResponse } from '../shared/Models/http';
 
 @Injectable({
   providedIn: 'root',
