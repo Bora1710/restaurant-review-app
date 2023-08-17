b013d79151686e041c86e75accc23b6683c983ba
Bora1710 <milosevicb92@gmail.com>
2023-08-17 18:38:04 +0200

    Add form for new restaurant

--- src/app/newrestaurant/newrestaurant.component.ts
+++ src/app/newrestaurant/newrestaurant.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-newrestaurant',
   templateUrl: './newrestaurant.component.html',
-  styleUrls: ['./newrestaurant.component.css']
+  styleUrls: ['./newrestaurant.component.css'],
 })
 export class NewrestaurantComponent {
-
+  restaurantForm = new FormGroup({
+    name: new FormControl<string>(''),
+    description: new FormControl<string>(''),
+  });
 }
