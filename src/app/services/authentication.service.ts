2d7ece9094cfe47e3bb24c4dcf318f1279275256
Bora1710 <milosevicb92@gmail.com>
2023-08-17 16:24:45 +0200

    Create constants for api urls

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,31 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
 import { HttpResponse } from '../shared/Models/http';
+import { url } from '../shared/constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  baseUrl = 'http://localhost:8080/register';
+  private loginUrl = url.loginUrl;
+  private registerUrl = url.registerUrl;
 
   constructor(private http: HttpClient) {}
 
   login(userName: string, password: string) {
     let loginData = { userName: userName, password: password };
-    return this.http
-      .post<HttpResponse>(`http://localhost:8080/login`, loginData)
-      .pipe(
-        map((response) => {
-          if (response.isSuccess) {
-            return response.payLoad;
-          }
-        })
-      );
+    return this.http.post<HttpResponse>(`${this.loginUrl}`, loginData).pipe(
+      map((response) => {
+        if (response.isSuccess) {
+          return response.payLoad;
+        }
+      })
+    );
   }
 
   register(userName: string, password: string, role: number) {
     let loginData = { userName: userName, password: password, role: role };
-    return this.http.post<HttpResponse>(`${this.baseUrl}`, loginData).pipe(
+    return this.http.post<HttpResponse>(`${this.registerUrl}`, loginData).pipe(
       map((response) => {
         if (response.isSuccess) {
           return response.payLoad;
