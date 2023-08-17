c5e35f584435957bedef3ac2e38d588cc44f0543
Bora1710 <milosevicb92@gmail.com>
2023-08-17 17:58:53 +0200

    add routes for newrestaurant and restaurantlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RestaurantlistComponent } from './restaurantlist/restaurantlist.component';
+import { NewrestaurantComponent } from './newrestaurant/newrestaurant.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'restaurant-list', component: RestaurantlistComponent },
+  { path: 'new-restaurant', component: NewrestaurantComponent },
 ];
 
 @NgModule({
--- src/app/restaurantlist/restaurantlist.component.ts
+++ src/app/restaurantlist/restaurantlist.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-restaurantlist',
   templateUrl: './restaurantlist.component.html',
-  styleUrls: ['./restaurantlist.component.css']
+  styleUrls: ['./restaurantlist.component.css'],
 })
 export class RestaurantlistComponent {
-
+  
 }
