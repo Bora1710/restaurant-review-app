39aca7543fa7f54958f51791cad69bf5d63a8f8d
Bora1710 <milosevicb92@gmail.com>
2023-08-19 13:37:13 +0200

    Create restaurant and review interfaces

---
+++ src/app/shared/Models/restaurant.ts
@@ -0,0 +1,16 @@
+export interface Restaurant {
+  id?: string;
+  name: string;
+  description: string;
+  dateOfCreation?: Date;
+  reviews?: Review[];
+  averageRating?: number;
+}
+
+export interface Review {
+  id: string;
+  rating: number;
+  dateOfVisit: Date;
+  comment: string;
+  commentedByUserId: string;
+}
