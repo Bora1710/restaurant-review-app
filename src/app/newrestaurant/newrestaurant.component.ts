0bfda0cf028c09626bce2e8fa0125bdc3f72c326
Bora1710 <milosevicb92@gmail.com>
2023-08-21 15:15:17 +0200

    Add router from create new to restaurant list

--- src/app/newrestaurant/newrestaurant.component.ts
+++ src/app/newrestaurant/newrestaurant.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { RestaurantService } from '../services/restaurant.service';
 import { Restaurant } from '../shared/Models/restaurant';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-newrestaurant',
@@ -14,14 +15,19 @@ export class NewrestaurantComponent {
     description: new FormControl(''),
   });
 
-  constructor(private restaurantService: RestaurantService) {}
+  constructor(
+    private restaurantService: RestaurantService,
+    private router: Router
+  ) {}
 
   onSubmit() {
     if (this.restaurantForm.valid) {
       let value: Restaurant = this.restaurantForm.value as Restaurant;
       value.dateOfCreation = new Date();
 
-      this.restaurantService.newRestaurant(value).subscribe((payLoad) => {});
+      this.restaurantService.newRestaurant(value).subscribe((payLoad) => {
+        payLoad ? this.router.navigate(['/restaurant-list']) : null;
+      });
     }
   }
 }
