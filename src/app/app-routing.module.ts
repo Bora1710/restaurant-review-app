463315ae3c61fc09a06cf5683770d8a50541a114
Bora1710 <milosevicb92@gmail.com>
2023-08-22 18:35:06 +0200

    Add routes from restaurant list to single restaurant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RestaurantlistComponent } from './restaurantlist/restaurantlist.component';
 import { NewrestaurantComponent } from './newrestaurant/newrestaurant.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {
-    path: 'restaurants', 
+    path: 'restaurants',
     children: [
-      { path: '', component:RestaurantlistComponent},
-      { path: 'new', component: NewrestaurantComponent }
-    ]
+      { path: '', component: RestaurantlistComponent },
+      { path: 'new', component: NewrestaurantComponent },
+      { path: ':id', component: RestaurantComponent },
+    ],
   },
 ];
 
