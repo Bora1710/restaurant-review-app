55e6f18c687201619d5206ae46425f4883f76b19
Bora1710 <milosevicb92@gmail.com>
2023-09-06 14:13:23 +0200

    add url for user

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -2,4 +2,5 @@ export const url = {
   loginUrl: 'http://localhost:8080/login',
   registerUrl: 'http://localhost:8080/register',
   restaurantsUrl: 'http://localhost:8080/restaurants',
+  userUrl: 'http://localhost:8080/user',
 };
