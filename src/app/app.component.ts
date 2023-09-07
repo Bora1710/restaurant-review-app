fc45706f8ec957eb6c147d522d0e6ef5accb5596
Bora1710 <milosevicb92@gmail.com>
2023-09-07 16:18:06 +0200

    WIP: checking are you logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,17 @@ import { AuthenticationService } from './services/authentication.service';
 })
 export class AppComponent {
   title = 'Restaurant-review-app';
-  constructor(private authService: AuthenticationService) {
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {
     if (localStorage.getItem('token')) {
       this.authService
         .userInfoFromToken(localStorage.getItem('token')!)
         .subscribe();
+    } else {
+      alert('You have to login first!');
+      this.router.navigate(['']);
     }
   }
 }
