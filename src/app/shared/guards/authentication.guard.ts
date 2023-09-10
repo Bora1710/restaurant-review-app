31ae7aa3d57ec8d781975207756523a7b098632c
Bora1710 <milosevicb92@gmail.com>
2023-09-10 10:45:23 +0200

    Delete unused types and imports

--- src/app/shared/guards/authentication.guard.ts
+++ src/app/shared/guards/authentication.guard.ts
@@ -1,12 +1,10 @@
-import { Injectable, ɵbypassSanitizationTrustStyle } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
   Router,
   RouterStateSnapshot,
-  UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Injectable({
@@ -21,11 +19,7 @@ export class AuthenticationGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  ): boolean {
     return this.authenticate();
   }
 
