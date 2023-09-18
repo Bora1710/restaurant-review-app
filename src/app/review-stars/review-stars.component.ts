0f9577bd7977ccb0af9342b9aa5dda16d6bd1a7c
Bora1710 <milosevicb92@gmail.com>
2023-09-18 19:14:32 +0200

    Move elements from parent to child component

--- src/app/review-stars/review-stars.component.ts
+++ src/app/review-stars/review-stars.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Restaurant } from '../shared/Models/restaurant';
 
 @Component({
   selector: 'app-review-stars',
   templateUrl: './review-stars.component.html',
-  styleUrls: ['./review-stars.component.css']
+  styleUrls: ['./review-stars.component.css'],
 })
 export class ReviewStarsComponent {
+  @Input() restaurant: Restaurant = new Restaurant();
 
+  stars = [1, 2, 3, 4, 5];
 }
