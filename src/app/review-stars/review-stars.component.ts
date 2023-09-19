c78415abe30e5e568d1de12455b1a7155e66de0b
Bora1710 <milosevicb92@gmail.com>
2023-09-19 16:02:26 +0200

    Logic for fill stars

--- src/app/review-stars/review-stars.component.ts
+++ src/app/review-stars/review-stars.component.ts
@@ -9,5 +9,25 @@ import { Restaurant } from '../shared/Models/restaurant';
 export class ReviewStarsComponent {
   @Input() restaurant: Restaurant = new Restaurant();
 
-  stars = [1, 2, 3, 4, 5];
+  getFullStars(): number[] {
+    return Array(Math.floor(this.restaurant.averageRating || 0)).fill(0);
+  }
+
+  hasHalfStar(): boolean {
+    if (this.restaurant.averageRating) {
+      return this.restaurant.averageRating % 1 !== 0.0;
+    } else {
+      return false;
+    }
+  }
+
+  getNonFilledStars(): number[] {
+    if (this.restaurant.averageRating) {
+      const nonFilledCount = 5 - Math.ceil(this.restaurant.averageRating || 5);
+      return Array(nonFilledCount).fill(0);
+    }
+    else {
+      return Array(5).fill(0);
+    }
+  }
 }
